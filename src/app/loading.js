e82ecf156000934dfe793c08acdc5824494e384f
Iftekhar-007 <shawonsp534@gmail.com>
2025-08-26 19:36:06 +0600

    okay

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,14 +1,3 @@
-// export default function Loading() {
-//   return (
-//     <div className="flex items-center justify-center min-h-screen">
-//       <div className="text-center">
-//         <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-blue-500 mx-auto mb-4"></div>
-//         <h2 className="text-xl font-semibold"></h2>
-//       </div>
-//     </div>
-//   );
-// }
-
 export default function Loading() {
   return (
     <div className="flex items-center justify-center min-h-screen">
